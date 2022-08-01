af26dddcf46ac61a07094626e9f058bdc0243953
Crystal Koo <96crystal.k@gmail.com>
2022-08-01 19:51:06 +0800

    add sum component

---
+++ src/lib/TotalExpenses.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { expenses } from "../stores/expensesData.js";
+
+    $: totalExpenses = $expenses.reduce((acc, curr) => acc + curr.amount, 0);
+</script>
+
+<h2
+    class="sticky top-10 h-45 text-slate-100 text-2xl w-full md:max-w-2xl py-10 px-6 sm:px-10 md:px-0 mb-10"
+>
+    Total Expenses: ${totalExpenses}
+</h2>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,8 @@
 <script>
-    import { expenses } from "../stores/expensesData.js";
+    import TotalExpenses from "$lib/TotalExpenses.svelte";
     import ExpensesList from "$lib/ExpenseList.svelte";
-
-    $: totalExpenses = $expenses.reduce((acc, curr) => acc + curr.amount, 0);
 </script>
 
-<h2
-    class="sticky top-10 h-45 text-slate-100 text-2xl w-full md:max-w-2xl py-10 px-6 sm:px-10 md:px-0 mb-10"
->
-    Total Expenses: ${totalExpenses}
-</h2>
+<TotalExpenses />
 
 <ExpensesList />
