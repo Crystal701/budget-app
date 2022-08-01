944258ab6e35ecfe1c43d0b6247dc016a7420059
Crystal Koo <96crystal.k@gmail.com>
2022-08-01 19:04:24 +0800

    fix sticky positions

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -21,7 +21,7 @@
         >
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 inline-block"
+                class="h-4 w-4 hidden sm:inline-block"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
